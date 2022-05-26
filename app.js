b9ddc01412b47c54d9159135850246e4d604ac7e
ZeroM2 <kemy21@naver.com>
2022-05-27 01:17:57 +0900

    수정 API 구현

--- app.js
+++ app.js
@@ -52,6 +52,60 @@ app.get("/ws", async (req, res) => {
   });
 });
 
+app.patch("/ws/:id", async (req, res) => {
+  const { id } = req.params;
+  const [[saying_dateRow]] = await pool.query(
+    `
+    SELECT *
+    FROM ws
+    WHERE id = ?
+    `,
+    [id]
+  );
+
+  if (Saying_dateRow === undefined) {
+    res.status(404).json({
+      resultCode: "F-1",
+      msg: "404 not found",
+    });
+    return;
+  }
+
+  const {
+    content = Saying_dateRow.content,
+    human = Saying_dateRow.human,
+    likeCount = Saying_dateRow.likeCount,
+    badCount = Saying_dateRow.badCount,
+  } = req.body;
+
+  await pool.query(
+    `
+    UPDATE ws
+    SET content = ?,
+    human = ?,
+    likeCount = ?,
+    badCount = ?
+    WHERE id = ?
+    `,
+    [content, human, likeCount, badCount, id]
+  );
+
+  const [[justModifiedWiseSayingRow]] = await pool.query(
+    `
+    SELECT *
+    FROM ws
+    WHERE id = ?
+    `,
+    [id]
+  );
+
+  res.json({
+    resultCode: "S-1",
+    msg: "성공",
+    data: justModifiedWiseSayingRow,
+  });
+});
+
 app.listen(port, () => {
   console.log(`wise_saying app listening on port ${port}`);
 });
